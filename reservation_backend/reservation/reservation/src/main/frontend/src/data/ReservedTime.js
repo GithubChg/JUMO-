edb4a4d6a63d5771a5edaa1ba79cb2dda7cec6d6
wkkyu088 <dnjsrbwls08@gmail.com>
2022-11-29 02:43:12 +0900

    ReservedTime.js 다시 수정

--- reservation_backend/reservation/reservation/src/main/frontend/src/data/ReservedTime.js
+++ reservation_backend/reservation/reservation/src/main/frontend/src/data/ReservedTime.js
@@ -12,17 +12,17 @@ axios({
     for (var k = 0; k<ReservedTime.length; k++) {
         ReservedTime[k] = new Array(TimeList.length).fill(0);
     }
-    
-    const ls = []
-    for (var i=0; i<data.length; i++) {
-        ls.push({
-            date: data[i].reservationDate.substr(0,10),
-            time: TimeList.indexOf(data[i].reservationDate.substr(11))
-        })
-    }
-    console.log(ls)
 
-    if (data.length!==0) {
+    if (data) {
+        const ls = []
+        for (var i=0; i<data.length; i++) {
+            ls.push({
+                date: data[i].reservationDate.substr(0,10),
+                time: TimeList.indexOf(data[i].reservationDate.substr(11))
+            })
+        }
+        console.log(ls)
+
         const dateNow = new Date();
         const today = dateNow.getFullYear()+"-"+(dateNow.getMonth()+1)+"-"+dateNow.getDate();
         const now = new Date(today.slice(0,4), today.slice(5,7), today.slice(8,10));
@@ -32,7 +32,6 @@ axios({
             ReservedTime[diff/(1000 * 60 * 60 * 24)][ls[j].time] = 1
         }
     }
-
     console.log(ReservedTime)
 })
 
