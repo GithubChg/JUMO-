e3d6794e80e9af7c0aac75b569e070f8631b863b
JUNGJUNH <101854651+JUNGJUNH@users.noreply.github.com>
2022-11-28 09:40:26 +0900

    관리자의 메뉴 변경화면 수정 재업로드

--- reservation/src/main/frontend/src/pages/MMenuModify.js
+++ reservation/src/main/frontend/src/pages/MMenuModify.js
@@ -4,38 +4,55 @@ import Header from '../components/Header';
 import {MenuItem, FormControl, Select, Stack, TextField, Grid} from '@mui/material';
 import TimeList from "../data/TimeList.js";
 import MenuList from "../data/MenuList.js";
-import MenuModal from '../components/MenuModal';
 import "./css/styles.css";
 import Logout from '../components/Logout';
 
-function MMenuModify(props) {
+function MMenuModify() {
+    const navigate = useNavigate();
+
     const location = useLocation();
     
-    const _id = location.state.id;
     const _name = location.state.name;
+    const _menu = location.state.menu;
     const _name1 = location.state.name1;
     const _price = location.state.price;
     const _people = location.state.people;
-
+    const _peopleCnt = location.state.people;
 
     console.log({
-        id: _id,
-        name: _name,
-        name1: _name1,
-        price: _price,
-        people: _people,
+        peopleCnt: _peopleCnt,
     })
 
-    // 예약 날짜 설정
-  
+    const [name, setName] = useState(_name);
+    const [menu, setMenu] = useState(_menu);
+    const [name1, setName1] = useState(_name1);
+    const [price, setPrice] = useState(_price);
+    const [people, setpeople] = useState(_people);
+
+    const [peopleCnt, setPeopleCnt] = useState(_peopleCnt);
+    const peopleList = [
+        1, 2, 3, 4, 5, 6, 7, 8
+    ];
+
+
+    const onClickCreate = () => {
+        navigate("/manager/mmenucheck", {state: {
+            menu: menu,
+            name: name,
+            name1: name1,
+            price: price,
+            people:people,
+        }});
+    };
+
     return (
         <>
-            <Header isManager={true} />
+        <Header isManager={true} />
             <div className="box">
                 <Stack className="menu" justifyContent="space-between">
                     <div>
                         <div className="menuGroup">예약 관리</div>
-                        <div className="menuBtn on">
+                        <div className="menuBtn">
                             <Link to="/manager/resvview" className="menuLink">예약 조회/변경</Link>
                         </div>
                         <div className="menuBtn">
@@ -45,78 +62,101 @@ function MMenuModify(props) {
                         <div className="menuBtn">
                             <Link to="/manager/mtimemodify" className="menuLink">운영 시간 변경</Link>
                         </div>
-                        <div className="menuBtn">
+                        <div className="menuBtn on">
                             <Link to="/manager/mmenuview" className="menuLink">메뉴 조회/변경</Link>
                         </div>
                     </div>
                     <Logout />
                 </Stack>
 
+                    
                 <div className="content">
-                    <div className="guide">변경할 메뉴 정보를 입력하세요</div>
-                    <div className="title">예약 정보</div>
-                  
-                   
-             
-            
-                    {/* <Stack direction="row" alignItems="start" className="subcontent" mt={2}>
-                        <div>
-                            <div className="subtitle">메뉴<span style={{fontSize:'14px', color:'red'}}> *</span></div>
-                            <div style={{color: "#5B9BD5", fontSize: "12px", marginTop: '10px'}}>총 {price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원</div>
-                            <button 
-                                style={{marginTop: '10px', backgroundColor: 'white', fontSize: '12px', border: '1px solid #BFBFBF', borderRadius: '5px'}} 
-                                onClick={() => {
-                                    setMenuCnt(Array(MenuList.length).fill(0));
-                                    setPrice(0);
-                                }}>초기화</button>
-                        </div>
-                        <Grid container rowSpacing={2} columnSpacing={1}>
-                        {
-                            MenuList.map((item, idx) => (
-                                <Grid item key={idx}>
-                                    <Stack>
-                                        <span style={{fontSize: '14px', marginBottom: '5px'}}>{item[0]}</span>
-                                        <span style={{fontSize: '12px', marginBottom: '10px'}}>{item[1].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원</span>
-                                        <div style={{width: '120px', height: '85px', backgroundColor: 'transparent'}} />
-                                        <img className="menuImage" src={`/img/img${idx+1}.jpg`} alt={`메뉴 이미지${idx+1}`} title="자세히보기" />
-                                        <MenuModal name={MenuList[idx][0]} price={MenuList[idx][1]} detail={MenuList[idx][2]} src={`/img/img${idx+1}.jpg`} />
-                                        <Stack direction="row" justifyContent="center" alignItems="center" spacing={1}>
-                                            <button 
-                                                name="menu"
-                                                style={{width: '22px', height: '22px', color: 'white', backgroundColor: '#5B9BD5', border: 'none', borderRadius: '20px', cursor: 'pointer'}}
-                                                onClick={() => {
-                                                    setMenuCnt(menuCnt.map(
-                                                        (cnt, i) => (i===idx)&&(cnt!==0) ? cnt-1 : cnt
-                                                    ))
-                                                    setPrice(price-MenuList[idx][1])
-                                                }}
-                                            >-</button>
-                                            <div style={{padding: '6px 14px', fontSize: '14px', border: '1px solid #BFBFBF', borderRadius: '5px'}}>{menuCnt[idx]}</div>
-                                            <button 
-                                                name="menu"
-                                                style={{width: '22px', height: '22px', color: 'white', backgroundColor: '#5B9BD5', border: 'none', borderRadius: '20px', cursor: 'pointer'}}
-                                                onClick={() => {
-                                                    setMenuCnt(menuCnt.map(
-                                                        (cnt, i) => (i===idx)&&(cnt!==5) ? cnt+1 : cnt
-                                                    ))
-                                                    setPrice(price+MenuList[idx][1])
-                                                }}
-                                            >+</button>
-                                        </Stack>
-                                    </Stack>
-                                </Grid>
-                        ))}
-                        </Grid>
+                    <div className="guide">변경할 예약 정보를 입력하세요</div>
+    <Stack direction="row" alignItems="center" className="subcontent">
+        <div className="subtitle">이름<span style={{fontSize:'14px', color:'red'}}> </span></div>
+    <TextField 
+        label="" 
+        variant="outlined" 
+        size="small" 
+        style={{width: '120px'}} 
+        inputProps={{style: {fontSize: '14px'}}} 
+        value={name}
+        onChange={(e) => {
+            if(e.target.value.length <= 10) {
+                setName(e.target.value)
+            }
+        }}
+         />
+</Stack>
+
+<Stack direction="row" alignItems="start" className="subcontent">
+    <div className="subtitle" style={{marginTop: '5px'}}>설명</div>
+    <TextField 
+        label="" 
+        multiline 
+        rows={4} 
+        size="small" 
+        style={{width: '600px'}} 
+        inputProps={{style: {fontSize: '14px'}}} 
+        value={name1}
+        onChange={(e) => {
+            if(e.target.value.length <= 200) {
+                setName1(e.target.value)
+            }
+        }}
+        placeholder="요청사항을 적어주세요" />
+</Stack>
+
+
+<Stack direction="row" alignItems="center" className="subcontent">
+    <div className="subtitle">가격<span style={{fontSize:'14px', color:'red'}}> </span></div>
+    <TextField 
+        label="" 
+        variant="outlined" 
+        size="small" 
+        style={{width: '150px'}} 
+        inputProps={{style: {fontSize: '14px'}}} 
+        value={price}
+        onChange={(e) => {
+            if(e.target.value.length <= 10) {
+                setPrice(e.target.value)
+            }
+        }}
+        placeholder="" />
+          <span style={{fontSize: '14px', margin: '0 0 0 5px'}}>원</span>
+</Stack>
+
+
+<Stack direction="row" alignItems="center" className="subcontent">
+                        <div className="subtitle">재고<span style={{fontSize:'14px', color:'red'}}> </span></div>
+                        <FormControl sx={{ minHeight: 25, minWidth: 60 }}>
+                            <Select
+                                labelId="peopleCnt-label"
+                                id="peopleCnt"
+                                value={peopleCnt}
+                                label=""
+                                autoWidth
+                                onChange={(e) => setPeopleCnt(e.target.value)}
+                                size="small"
+                                style={{fontSize: '14px'}}
+                            >
+                                {
+                                    peopleList.map((item) => (
+                                    <MenuItem value={item} key={item} style={{fontSize: '14px'}}>{item}</MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                        <span style={{fontSize: '14px', margin: '0 0 0 5px'}}>개</span>
                     </Stack>
-                    {emptyPrice ? <span className="error">*메뉴를 선택하세요</span> : null}
-                    <div style={{marginLeft: '45%', marginTop: '50px', marginBottom: '50px'}}>
-                        <button className="btn" onClick={onClickModify}>변경</button>
-                    </div> */}
+
+
+                    <div style={{marginLeft: '45%', marginTop: '50px', marginBottom: '50px' }}>
+                        <button className="btn" onClick={onClickCreate}>변경</button>
+                    </div>
                 </div>
             </div>
         </>
     );
 }
-
 
 export default MMenuModify;
